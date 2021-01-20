3248746004042688a81dd70caa7f2feba71f4fdb
Laperrouse Theo <theo.laperrouse@etudiant.univ-rennes1.fr>
2021-01-20 08:48:36 +0100

    add webhooks

---
+++ webHooks/deploy.sh
@@ -0,0 +1,13 @@
+# !/bin/sh
+# https://levelup.gitconnected.com/automated-deployment-using-docker-github-actions-and-webhooks-54018fc12e32
+#On close l'appli
+#lancer webhook -hooks /my/path/to/hooks/hooks.json -verbose
+docker-compose down
+docker-compose rm -f
+#On récupère la MAJ
+git pull
+#On build les images
+docker-compose build
+#Et on relance l'appli
+docker-compose up
+exit 0
